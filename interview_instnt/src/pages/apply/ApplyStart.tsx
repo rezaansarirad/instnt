4067731273663fef6c68e148194b934f2287f855
ansarirad <reza@devotel.com>
2026-01-28 16:27:51 +0330

    version 2 for apply

--- interview_instnt/src/pages/apply/ApplyStart.tsx
+++ interview_instnt/src/pages/apply/ApplyStart.tsx
@@ -1,5 +1,5 @@
 import { useSearchParams, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export function ApplyStart() {
   const [searchParams] = useSearchParams();
